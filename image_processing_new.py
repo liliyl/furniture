99b15b9e0e647f6a207337d2e94a64c9554160de
Lili Yao <Lil@Lilis-MacBook-Pro.local>
2015-10-01 13:21:23 -0700

    Add vec_info json building code

--- image_processing_new.py
+++ image_processing_new.py
@@ -84,106 +84,16 @@ def get_domi_color(paths, category):
                 domi_color = color
 
         if not flag:    
-            # new_path = 'wayfair/images/' + category + '/background/' + path
-            # skimage.io.imsave(new_path, image)
+            new_path = 'wayfair/images/' + category + '/background/' + path
+            skimage.io.imsave(new_path, image)
         else:
-            # new_path = 'wayfair/images/' + category + '/white/' + path
-            # skimage.io.imsave(new_path, image)
+            new_path = 'wayfair/images/' + category + '/white/' + path
+            skimage.io.imsave(new_path, image)
             domi_color_dict[path] = domi_color 
 
     print '# of grayscaled photos: ', gray
         
     return domi_color_dict
-
-
-
-def vectorize_color_distribution(paths, category):
-    
-    '''INPUT:
-            paths: list of strings
-                file paths
-       OUTPUT:
-            color_dist_dict: dictionary 
-                key: path 
-                value: vectorized color distribution (a 1D numpy array of 30 numbers representing RBG 
-                                                        intensities, 10 numbers for each color)
-    '''
-    
-    color_dist_dict = {}
-    
-    for path in paths:
-        
-        image = skimage.io.imread('wayfair/images/' + category + '/' + path)
-        image = transform.resize(image, (300,300,3))
-    
-        nrow, ncol, depth = image.shape 
-        white_pixel = np.array([ .99, .99, .99])
-        lst_of_pixels = np.array([image[irow][icol] for irow in range(nrow) for icol in range(ncol) if np.mean(image[irow][icol]>white_pixel) != 1])
-
-        red_values = lst_of_pixels[:,0]
-        green_values = lst_of_pixels[:,1]
-        blue_values = lst_of_pixels[:,2]
-
-        (n_red, bins) = np.histogram(red_values, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-        (n_green, bins) = np.histogram(green_values, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-        (n_blue, bins) = np.histogram(blue_values, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
-        
-        num_of_pixels = float(len(lst_of_pixels))
-        n_red = n_red / num_of_pixels
-        n_green = n_green / num_of_pixels
-        n_blue = n_blue / num_of_pixels
-
-        color_dist_vector = np.hstack((n_red, n_green, n_blue))
-        color_dist_dict[path] = color_dist_vector
-        
-    return color_dist_dict
-
-
-
-def clustering_with_color(color_dict, category, n_clusters=10, save_image=True, domi_color=True):
-    '''
-    Cluster by color & save files to different folders according to labels.
-
-    INPUT: 
-        color_dict: dictionary
-            * key: path
-            * value: dominant color (just one) or color distribution vector
-      
-    OUTPUT: 
-        cluster_label_dict: dictionary
-            * key: path
-            * value: cluster label
-        color_centroids: list
-            * index: label 
-            * value: centroid
-    '''
-
-    color_values = color_dict.values()
-    
-    color_reverse_dict = {}
-    for i, j in color_dict.iteritems():
-        color_reverse_dict[tuple(j)] = i 
-        
-    km_color = KMeans(n_clusters=n_clusters)
-    colors_labels =km_color.fit_predict(color_values)
-    
-    color_centroids = km_color.cluster_centers_
-             
-    cluster_label_dict = {}
-    for i, j in enumerate(color_values):
-        label = colors_labels[i]
-        path = color_reverse_dict[tuple(j)]
-        cluster_label_dict[path] = label
-        
-        if save_image:
-            image = skimage.io.imread('wayfair/images/' + category + '/' + path)
-            if domi_color:
-                new_path = 'wayfair/images/' + category + '/domi_color/' + str(label) + '/' + path
-            else:
-                new_path = 'wayfair/images/' + category + '/color_dist/' + str(label) + '/' + path
-            skimage.io.imsave(new_path, image)
-    
-    return cluster_label_dict, color_centroids, km_color
 
 
 
@@ -237,6 +147,131 @@ def image_featurizer(category, sub_dir='white', edge=False, pca=True):
         return feature_dict
 
 
+
+# ----------------------------------------------------------------------------
+# For new images: 
+# ----------------------------------------------------------------------------
+
+def get_domi_color_new_image(image, n_clusters=2):
+    
+    if len(image.shape) == 3:
+        image = transform.resize(image, (300,300,3))
+    else:
+        return -1
+
+    nrow, ncol, depth = image.shape 
+    lst_of_pixels = [image[irow][icol] for irow in range(nrow) for icol in range(ncol)]
+    kmean = KMeans(n_clusters=n_clusters)
+    kmean.fit_transform(lst_of_pixels)
+    domi_colors_all = kmean.cluster_centers_ 
+    white_color_arr = np.array([0.98, 0.98, 0.98])
+
+    domi_color = None
+    for color in domi_colors_all:
+        if np.mean(color > white_color_arr) != 1:
+            domi_color = color
+    return domi_color
+
+
+
+# ----------------------------------------------------------------------------
+# For trying out color distribution vectorizaiton: 
+# ----------------------------------------------------------------------------
+
+def vectorize_color_distribution(paths, category):
+    
+    '''INPUT:
+            paths: list of strings
+                file paths
+       OUTPUT:
+            color_dist_dict: dictionary 
+                key: path 
+                value: vectorized color distribution (a 1D numpy array of 30 numbers representing RBG 
+                                                        intensities, 10 numbers for each color)
+    '''
+    
+    color_dist_dict = {}
+    
+    for path in paths:
+        
+        image = skimage.io.imread('wayfair/images/' + category + '/' + path)
+        image = transform.resize(image, (300,300,3))
+    
+        nrow, ncol, depth = image.shape 
+        white_pixel = np.array([ .99, .99, .99])
+        lst_of_pixels = np.array([image[irow][icol] for irow in range(nrow) for icol in range(ncol) if np.mean(image[irow][icol]>white_pixel) != 1])
+
+        red_values = lst_of_pixels[:,0]
+        green_values = lst_of_pixels[:,1]
+        blue_values = lst_of_pixels[:,2]
+
+        (n_red, bins) = np.histogram(red_values, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+        (n_green, bins) = np.histogram(green_values, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+        (n_blue, bins) = np.histogram(blue_values, bins=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1])
+        
+        num_of_pixels = float(len(lst_of_pixels))
+        n_red = n_red / num_of_pixels
+        n_green = n_green / num_of_pixels
+        n_blue = n_blue / num_of_pixels
+
+        color_dist_vector = np.hstack((n_red, n_green, n_blue))
+        color_dist_dict[path] = color_dist_vector
+        
+    return color_dist_dict
+
+
+
+# ----------------------------------------------------------------------------
+# For testing vectorizing results by clustering: 
+# ----------------------------------------------------------------------------
+
+def clustering_with_color(color_dict, category, n_clusters=10, save_image=True, domi_color=True):
+    '''
+    Cluster by color & save files to different folders according to labels.
+
+    INPUT: 
+        color_dict: dictionary
+            * key: path
+            * value: dominant color (just one) or color distribution vector
+      
+    OUTPUT: 
+        cluster_label_dict: dictionary
+            * key: path
+            * value: cluster label
+        color_centroids: list
+            * index: label 
+            * value: centroid
+    '''
+
+    color_values = color_dict.values()
+    
+    color_reverse_dict = {}
+    for i, j in color_dict.iteritems():
+        color_reverse_dict[tuple(j)] = i 
+        
+    km_color = KMeans(n_clusters=n_clusters)
+    colors_labels =km_color.fit_predict(color_values)
+    
+    color_centroids = km_color.cluster_centers_
+             
+    cluster_label_dict = {}
+    for i, j in enumerate(color_values):
+        label = colors_labels[i]
+        path = color_reverse_dict[tuple(j)]
+        cluster_label_dict[path] = label
+        
+        if save_image:
+            image = skimage.io.imread('wayfair/images/' + category + '/' + path)
+            if domi_color:
+                new_path = 'wayfair/images/' + category + '/domi_color/' + str(label) + '/' + path
+            else:
+                new_path = 'wayfair/images/' + category + '/color_dist/' + str(label) + '/' + path
+            skimage.io.imsave(new_path, image)
+    
+    return cluster_label_dict, color_centroids, km_color
+
+
+
 def clustering_with_feature(feature_dict, category, n_clusters=10, pca=False, save_image=True, edge=False):
     '''
     Cluster by features & save files to different folders according to labels.
@@ -285,27 +320,3 @@ def clustering_with_feature(feature_dict, category, n_clusters=10, pca=False, sa
     return cluster_label_dict, feature_centroids, km_feature
 
 
-
-# ----------------------------------------------------------------------------
-# For new images: 
-# ----------------------------------------------------------------------------
-
-def get_domi_color_new_image(image, n_clusters=2):
-    
-    if len(image.shape) == 3:
-        image = transform.resize(image, (300,300,3))
-    else:
-        return -1
-
-    nrow, ncol, depth = image.shape 
-    lst_of_pixels = [image[irow][icol] for irow in range(nrow) for icol in range(ncol)]
-    kmean = KMeans(n_clusters=n_clusters)
-    kmean.fit_transform(lst_of_pixels)
-    domi_colors_all = kmean.cluster_centers_ 
-    white_color_arr = np.array([0.98, 0.98, 0.98])
-
-    domi_color = None
-    for color in domi_colors_all:
-        if np.mean(color > white_color_arr) != 1:
-            domi_color = color
-    return domi_color
