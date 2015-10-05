cca330beac156b0d8ef3408eae1fca65df082bd0
Lili Yao <Lil@Lilis-MacBook-Pro.local>
2015-10-05 10:50:12 -0700

    update to the web-app

--- web_app/app_furniture.py
+++ web_app/app_furniture.py
@@ -362,15 +362,15 @@ def init_server():
 
     for category in categories:
 
-        path = 'data/pickled_model/' + category + '_pca_scaler.pkl'
+        path = 'data/pickled_models/' + category + '_pca_scaler.pkl'
         with open(path) as f:
             f
 
-        path = 'data/pickled_model/' + category + '_pca_model.pkl'
+        path = 'data/pickled_models/' + category + '_pca_model.pkl'
         with open(path) as f:
             pca_model_dict[category] = pickle.load(f)
 
-        path = 'data/pickled_model/' + category + '_tfidf.pkl'
+        path = 'data/pickled_models/' + category + '_tfidf.pkl'
         with open(path) as f:
             tfidf_dict[category]  = pickle.load(f)
 
